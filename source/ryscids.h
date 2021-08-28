63157c3157eccbe5b4f96f016f25820e81ced486
Kvar_ispw17 <enkerewpo@gmail.com>
2021-08-28 21:00:22 +0800

    init

---
+++ source/ryscids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2021 rys.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace rys {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kmathsynthProcessorUID (0x44987C92, 0x59B75667, 0x9F9498E2, 0x0A742689);
+static const Steinberg::FUID kmathsynthControllerUID (0xF59EAEEE, 0xB82054F7, 0x957BC99F, 0x091E6501);
+
+#define mathsynthVST3Category "Instrument"
+
+//------------------------------------------------------------------------
+} // namespace rys

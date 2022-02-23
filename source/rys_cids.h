6264e38e31484f61bfcca949f2ba63410009423b
rys_ <enkerewpo@hotmail.com>
2022-02-23 21:48:49 +0800

    update force

---
+++ source/rys_cids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2021 rys.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace rys {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kmatsynProcessorUID (0xDA1D0743, 0xFA2B5A1D, 0x9B752478, 0xB829C861);
+static const Steinberg::FUID kmatsynControllerUID (0x9FCF2E5F, 0x33B05EE2, 0xA387894A, 0x8384A902);
+
+#define matsynVST3Category "Instrument"
+
+//------------------------------------------------------------------------
+} // namespace rys
